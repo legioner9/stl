a80ee51cefb9d566909241d546a0a6649ffa2e3f
[alt]legioner9 <legioner9@inbox.ru>
2024-10-20 20:49:07 +0700

    <> ##

--- .d/.ham/main/main.fn.lst.d/cp_dotstldotd.sh
+++ .d/.ham/main/main.fn.lst.d/cp_dotstldotd.sh
@@ -6,6 +6,7 @@ cp_dotstldotd() {
     diff -r ${HOME}/.stl.d ${REPO_PATH}/${name_repo}/.d/.stl.d || {
         is_yes_boot_stl "cp -rf ${HOME}/.stl.d ${REPO_PATH}/${name_repo}/.d/.stl.d" && {
             echo "exec: cp -fr ${HOME}/.stl.d ${REPO_PATH}/${name_repo}/.d/.stl.d"
+            mv ${REPO_PATH}/${name_repo}/.d/.stl.d ${REPO_PATH}/${name_repo}/.d/.stl.d~
             cp -fr ${HOME}/.stl.d ${REPO_PATH}/${name_repo}/.d/.stl.d || {
                 echo "in file://${REPO_PATH}/${name_repo}/.d/.ham/main/main.fn.lst.d/cp_dotstldotd.sh : FAIL_EXEC: cp -fr ${HOME}/.stl.d ${REPO_PATH}/${name_repo}/.d/.stl.d return 1" >&2
                 return 1
