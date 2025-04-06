a6b9bc1b79eaf1917ee828598ce237dfde610821
[alt]legioner9 <legioner9@inbox.ru>
2025-04-06 22:53:01 +0700

    <> see #2
    extend . _rbld_l_xx.sh name_fn for all

---
+++ .d/.stl.d/prc.d/boot_stl_fn.d_v1/__prc/_tst.prc
@@ -0,0 +1,14 @@
+
+#? for copy to help block
+# MAIN: ${FNN} :: 
+# TAGS:
+# \$1 
+# [, \$2]
+# CNTL: 
+#     -h : help
+#     _e : _edit body      : vim ${sh_file}
+# EXAM: 
+#     ${FNN}
+echo -e "${ECHO_EXEC}'$FNN $*'${NRM}"
+
+echo "true 3"
---
+++ .d/.stl.d/prc.d/boot_stl_fn.d_v1/l_01_stl_init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+### Written by Legioner9 for the universe
+### Part of the STL shell subsystem
+### Location ${STL_D_PATH}/prc.d/boot_stl_fn.d
+
+# ENV: ${STL_REPO_PATH} ${STL_D_PATH} ${STL_DATA_D_PATH} ${STL_LIB_D_PATH}
+# "'$FNN() $*' in file://${file_name} :: CAUS_NAME 'code' :: return 1" >&2
+
+l_01_stl_init() { # source .stldrc
+
+    local FNN=${FUNCNAME[0]}
+    local PPWD=$PWD
+    local file_name=${STL_D_PATH}/prc.d/boot_stl_fn.d/${FNN}.sh
+
+    if ! [[ -d "${PPWD}" ]]; then
+        echo -e "${ECHO_RET1}'$FNN() $*' in file://${file_name} , line=${LINENO} :: NOT_DIR [{PPWD}] '${PPWD}' return 1${NRM}" >&2
+        return 1
+    fi
+
+    if [[ "_e" == "$1" ]]; then
+        vim ${file_name}
+        return 0
+    fi
+
+    l_00_echo_exec "${FNN}() $*"
+
+    l_00_echo_code "source ${STL_D_PATH}/.stldrc"
+    source "${STL_D_PATH}/.stldrc"
+
+    cd "${PPWD}" || {
+        echo -e "${ECHO_RET1}'$FNN() $*' in file://${file_name} , line=${LINENO} :: NOT_DIR [{PPWD}] '${PPWD}' return 1${NRM}" >&2
+        return 1
+    }
+    return 0
+}
