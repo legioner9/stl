f595a73435dffa278724b0d4798265c4e79f898c
[alt]legioner9 <legioner9@inbox.ru>
2025-04-18 21:14:40 +0700

    <> ##

---
+++ .d/.stl.d/prc.d/boot_stl_fn.d/__prc/l_04_is_lfn.prc
@@ -0,0 +1,39 @@
+#? for copy to help block
+if [[ "-h" == "$1" ]]; then
+    echo -e "
+MAIN: ${FNN} :: 
+TAGS:
+\$1 
+[, \$2]
+CNTL: 
+
+    -h          : help
+    _go         : edit body     : l_02_edit ${fn_nm}
+    _rbld       : rebuild fn    : . ${fn_dr}/_rbld_l_xx.sh ${FNN}
+    _tst        : tst fn        : . ${tst_nm_dr}/${FNN}/exec.tst
+    _flow       : tst _flow     : . ${tst_nm_dr}/${FNN}/_flow_tst.sh
+    _flow_1     : tst _flow_1   : . ${tst_nm_dr}/${FNN}/_flow_tst_v1.sh
+
+    _e_prc      : edit fn.prc   : l_02_edit ${prc_nm}
+    _e_tst_dr   : edit tst_nm_dr: l_02_edit ${tst_nm_dr}
+    _e_xxx      : edit fl with \"init block\" for all fn : l_02_edit ${fn_dr}/l_04_is_lfn
+
+RETU: (any {0} | if: [...] {0} | if [...] {1} | result>stdout, return 0 | data | change to ptr |  fs_structure | ...)
+EXAM:   ${FNN} [, [, ]]
+"
+    cd "${PPWD}" || {
+        echo -e "${ECHO_RET1}'$FNN() $*' in file://${fn_nm} , line=${LINENO} :: NOT_DIR [{PPWD}] '${PPWD}' return 1${NRM}" >&2
+        return 1
+    }
+    return 0
+fi
+
+[[ -n "$1" ]] || {
+    l_00_echo_err "\$1 - EMPTY : use with \$1=l_xx_name"
+    return 1
+}
+
+
+
+#! stdout fn introduction
+# echo -e "${ECHO_EXEC}'$FNN $*'${NRM}"
--- .d/.stl.d/prc.d/boot_stl_fn.d/l_04_is_lfn.sh
+++ .d/.stl.d/prc.d/boot_stl_fn.d/l_04_is_lfn.sh
@@ -8,11 +8,11 @@
 # "${ECHO_ERR}$FNN() $*' in file://${file_name} , line=${LINENO} :: CAUS_NAME [VAR] 'code' :: return 1${NRM}" >&2
 # cd ${PPWD} || echo -e "${ECHO_WAR}'$FNN() $*' in file://${file_name} , line=${LINENO} :: NOT_DIR [\${PPWD}] '${PPWD}' return 1${NRM}" >&2
 
-l_02_arb_tst() {
+l_04_is_lfn() {
 
-    #! THIS FILE WILL BE CREATED `. ${STL_D_PATH}/prc.d/boot_stl_fn.d/_rbld_l_xx.sh l_02_arb_tst`
+    #! THIS FILE WILL BE CREATED `. ${STL_D_PATH}/prc.d/boot_stl_fn.d/_rbld_l_xx.sh l_04_is_lfn`
     #! All changes will be lost after rebuilding
-    #! To change this file use the following commands `l_02_arb_tst _e_prc` to edit "fn block", `l_02_arb_tst _e_xxx` to edit "init block"
+    #! To change this file use the following commands `l_04_is_lfn _e_prc` to edit "fn block", `l_04_is_lfn _e_xxx` to edit "init block"
 
     #* START init block from ${STL_D_PATH}/prc.d/boot_stl_fn.d/_\XXX ------------------
     local FNN=${FUNCNAME[0]}
@@ -120,7 +120,7 @@ l_02_arb_tst() {
     fi
 
     if [[ "_rbld" == "$1" ]]; then
-        #! rebuild fn : bcp && ord fn.sh from l_02_arb_tst.sh , cp fn.prc into fn.sh
+        #! rebuild fn : bcp && ord fn.sh from l_04_is_lfn.sh , cp fn.prc into fn.sh
         . ${fn_dr}/_rbld_l_xx.sh ${FNN}
         #! up to mem fn
         . ${fn_dr}/$1.sh
@@ -172,7 +172,7 @@ l_02_arb_tst() {
     # local ptr_path="$1"
     # ptr_path="$(l_01_abs_path "${PPWD}" "ptr_path")"
 
-    #* START fn block from from ${STL_D_PATH}/prc.d/boot_stl_fn.d/__prc/l_02_arb_tst.prc ------------------
+    #* START fn block from from ${STL_D_PATH}/prc.d/boot_stl_fn.d/__prc/l_04_is_lfn.prc ------------------
     #[[fn_body]]
 #? for copy to help block
 if [[ "-h" == "$1" ]]; then
@@ -186,16 +186,15 @@ CNTL:
     -h          : help
     _go         : edit body     : l_02_edit ${fn_nm}
     _rbld       : rebuild fn    : . ${fn_dr}/_rbld_l_xx.sh ${FNN}
-    _tst        : tst fn        : l_02_edit ${tst_nm_dr}/${FNN}/exec.tst
-
+    _tst        : tst fn        : . ${tst_nm_dr}/${FNN}/exec.tst
     _flow       : tst _flow     : . ${tst_nm_dr}/${FNN}/_flow_tst.sh
     _flow_1     : tst _flow_1   : . ${tst_nm_dr}/${FNN}/_flow_tst_v1.sh
-    
+
     _e_prc      : edit fn.prc   : l_02_edit ${prc_nm}
     _e_tst_dr   : edit tst_nm_dr: l_02_edit ${tst_nm_dr}
-    _e_xxx      : edit fl with \"init block\" for all fn : l_02_edit ${fn_dr}/l_02_arb_tst
+    _e_xxx      : edit fl with \"init block\" for all fn : l_02_edit ${fn_dr}/l_04_is_lfn
 
-RETU: (any {0} | if: [...] {0} | if [...] {1})
+RETU: (any {0} | if: [...] {0} | if [...] {1} | result>stdout, return 0 | data | change to ptr |  fs_structure | ...)
 EXAM:   ${FNN} [, [, ]]
 "
     cd "${PPWD}" || {
@@ -206,58 +205,7 @@ EXAM:   ${FNN} [, [, ]]
 fi
 
 #! stdout fn introduction
-echo -e "${ECHO_EXEC}'$FNN $*'${NRM}"
-
-unset IFS
-
-if [[ -d "$1" ]]; then
-    cd "$1" || {
-        l_00_echo_ret1 "'$FNN() $*' in file://${STL_D_PATH}/.stldrc , line=${LINENO} :: FAIL_EXEC :: 'cd $1' :: return 1" >&2
-        return 1
-    }
-else
-    l_00_echo_ret1 "'$FNN() $*' in file://${STL_D_PATH}/.stldrc , line=${LINENO} :: \$1='$1' is not dir" >&2
-    return 1
-fi
-
-local dir=
-local item=
-local item_path=
-local err_flag=0
-
-dir=$(pwd)
-
-for item in *; do
-    # echo "\$item=$item"
-    [[ "_" == ${item:0:1} ]] && continue
-    # /lib.0stl.arb/_XXX_YYY.ram/.grot/_tst/_exec.tst
-    item_path=$dir/$item
-    item_name="${item%.*}"
-    item_ext="${item##*.}"
-    file_path=${item_path}/.grot/_tst/exec.tst
-
-    echo -e "${FG}\$file_path= file://$file_path${NRM}"
-
-    if [ -f "$file_path" ]; then
-        # echo ". $file_path"
-        . "$file_path" || {
-            l_00_echo_err "'$FNN() $*' in file://${STL_D_PATH}/.stldrc , line=${LINENO} :: EXEC_FAIL '. file://$file_path' :: err_flag=1" >&2
-            cd "$PPWD" || l_00_echo_err "'$FNN() $*' in fs= file://${STL_D_PATH}/.stldrc , line=${LINENO} , EXEC_FAIL : 'cd $PPWD' : continue" >&2
-            err_flag=1
-        }
-    fi
-done
-
-if [[ 1 -eq ${err_flag} ]]; then
-    l_00_echo_ret1 "in lib_dir= file://$1 FAIL_TST" >&2
-    cd "$PPWD" || l_00_echo_err "'$FNN() $*' in fs= file://${STL_D_PATH}/.stldrc , line=${LINENO} , EXEC_FAIL : 'cd $PPWD' : continue" >&2
-    return 1
-else
-    l_00_echo_info "in lib_dir= file://$1 TRUE_TST"
-    cd "$PPWD" || l_00_echo_err "'$FNN() $*' in fs= file://${STL_D_PATH}/.stldrc , line=${LINENO} , EXEC_FAIL : 'cd $PPWD' : continue" >&2
-    return 0
-fi
-
+# echo -e "${ECHO_EXEC}'$FNN $*'${NRM}"
 
     #* END fn block ------------------
 
@@ -270,4 +218,4 @@ fi
 }
 
 #! SELF EXEC
-# l_02_arb_tst @
+# l_04_is_lfn @
