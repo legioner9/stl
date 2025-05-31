d59c4c3f0536622689f92bd0cdd4062ff94a9456
[alt]legioner9 <legioner9@inbox.ru>
2025-05-31 17:48:50 +0700

    <> ##

---
+++ .d/.stl.d/prc.d/boot_stl_fn.d/__tst/l_02_su_lwc/_flow_tst.sh
@@ -0,0 +1,93 @@
+#!/usr/bin/env bash
+
+### Written by Legioner9 for the universe
+### Part of the STL shell subsystem
+### Location ${STL_D_PATH}/prc.d/boot_stl_fn.d
+
+# ENV: ${STL_REPO_PATH} ${STL_D_PATH} ${STL_DATA_D_PATH} ${STL_LIB_D_PATH}
+# "'$FNN() $*' in file://${file_name} :: CAUS_NAME 'code' :: return 1" >&2
+
+_l_02_su_lwc_RND_flow() {
+
+    unset IFS
+
+    local PW=$(pwd)
+
+    if ! command -v l_02_su_lwc >/dev/null; then
+        l_00_echo_ret1 "TYPE_ERROR : l_02_su_lwc"
+        cd $PW
+        return 1
+    fi
+
+    local filename=${STL_D_PATH}/prc.d/boot_stl_fn.d/__tst/l_02_su_lwc/_flow_tst.sh
+
+    local PW=$(pwd)
+    local idir="$(dirname ${filename})"
+    local tst_dir=${idir}/tst_dir
+    local res=${idir}/res
+
+    local ARGS0="$1"
+    local ARGS1="$2"
+    local ARGS2="$3"
+    local ARGS3="$4"
+
+    local NARGS=$#
+
+    cd "${idir}" || {
+        l_00_echo_ret1 "${idir} not dir"
+        return 1
+    }
+
+    #?----------------------------------------------------
+    #?-------------------------------------
+
+    l_00_echo_exec "source file://$filename"
+
+    #! if stdout to ${res} only in this file - NOT in exec.tst
+    : >"${res}"
+
+    cd tst_dir || {
+        hint="\$1: \$2: "
+        l_00_echo_ret1 "in fs= file://${filename} , line=${LINENO}, ${FNN}() : NOT_DIR : 'file://${idir}/_dir_tst' : ${hint} : return 1"
+        return 1
+    }
+
+    # l_02_su_lwc "${tst_dir}" &>>"${res}"
+    #! rm abs_path
+    # l_02_s2f "${STL_D_PATH}" '${STL_D_PATH}' "${res}"
+
+    #! stdin_tst
+    #         echo "!STRING_AS_WORD!"
+    # : >FILE
+    #     cat >FILE <<EOF
+    # third_str
+    # fourth_str
+    # EOF
+
+    #     echo "FILE_ARG:"
+    #     l_02_su_lwc FILE &>>"${res}"
+
+    #     echo "FILE_PIPE:"
+    #     cat FILE | l_02_su_lwc &>>"${res}"
+
+    #     echo "HERE_DOC:"
+    #     l_02_su_lwc <<EOF &>>"${res}"
+    # first_str
+    # second_str
+    # EOF
+
+    #     echo "HERE_STR:"
+    #     l_02_su_lwc <<<"here_str_::_fifth_str" &>>"${res}"
+    l_02_su_lwc " ew ERQV ddW" &>>"${res}"
+
+    #?-------------------------------------
+    #?----------------------------------------------------
+
+    cd "${PW}" || {
+        l_00_echo_ret1 "${PW} not dir"
+        return 1
+    }
+
+}
+
+_l_02_su_lwc_RND_flow "$@"
