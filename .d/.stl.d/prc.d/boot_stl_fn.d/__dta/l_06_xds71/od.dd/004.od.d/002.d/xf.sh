9b2e4148c74e82576771e73c8c810a85cf5e3b69
[alt]legioner9 <legioner9@inbox.ru>
2025-06-21 09:48:03 +0700

    <> ##

--- .d/.stl.d/prc.d/boot_stl_fn.d/__dta/l_06_xds71/od.dd/004.od.d/002.d/xf.sh
+++ .d/.stl.d/prc.d/boot_stl_fn.d/__dta/l_06_xds71/od.dd/004.od.d/002.d/xf.sh
@@ -4,7 +4,10 @@
 # first free arg ARGS[2] is $3 from l_02_ixfv
 # $PPWD from clothure of l_02_ixfv
 # num from pre_exec_file
-# echo file://$(pwd)/"${line}"
+local pml_jar=${STL_D_PATH}/data.d/cdn.d/plantuml.jar
 
-echo "${num} : ${ARGS[2]}${line}"
-num=$((num + 1))
+
+: >${tml_dir_pth}/u.1
+# l_00_echo_code "ls -A ${ARGS[2]} :: " | tee -a ${tml_dir_pth}/u.1
+l_00_echo_code "java -jar ${pml_jar} :: " | tee -a ${tml_dir_pth}/u.1
+java -jar ${pml_jar} -filename ${ARGS[2]} -o ${ARGS[3]}  ${ARGS[4]}| tee -a ${tml_dir_pth}/u.1
