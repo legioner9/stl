e95b0011e3cf79fdff0502f9e4edd458141f2840
[alt]legioner9 <legioner9@inbox.ru>
2025-06-28 12:15:45 +0700

    <> ##

--- .d/.stl.d/prc.d/boot_stl_fn.d/__dta/l_06_xds71/od.dd/002.od.d/001.d/.d/_tst/_flow_tst_v2.sh
+++ .d/.stl.d/prc.d/boot_stl_fn.d/__dta/l_06_xds71/od.dd/002.od.d/001.d/.d/_tst/_flow_tst_v2.sh
@@ -7,7 +7,7 @@
 # ENV: ${STL_REPO_PATH} ${STL_D_PATH} ${STL_DATA_D_PATH} ${STL_LIB_D_PATH}
 # "'$FNN() $*' in file://${file_name} :: CAUS_NAME 'code' :: return 1" >&2
 
-_001_d221b8d_flow_1() {
+_001_de47735_flow_1() {
 
     unset IFS
 
@@ -90,4 +90,4 @@ _001_d221b8d_flow_1() {
 
 }
 
-_001_d221b8d_flow_1 "$@"
+_001_de47735_flow_1 "$@"
